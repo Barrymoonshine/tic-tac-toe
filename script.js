e58c5562ebca2de226582b6bd48d5814b401e677
Nick Sheward <nickshwrd@gmail.com>
2023-02-17 14:49:04 +0000

    Counter updated to global scope

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const gameBoardContainer = document.getElementById("game-board-container");
 let count = 0;
 
-const createGameBoard = (function () {
-  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+// Module pattern to generate game board array
+const gameBoard = (function () {
+  const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
   return {
-    gameBoard,
+    gameBoardArray,
   };
 })();
 
+// Factory function to generate players
 const playerFactory = (name, marker) => ({ name, marker });
-
 const playerOne = playerFactory("playerOne", "X");
-
 const playerTwo = playerFactory("playerTwo", "O");
 
 function renderGameBoard() {
@@ -19,29 +19,32 @@ function renderGameBoard() {
     const boardSquare = document.createElement("div");
     boardSquare.className = "board-squares";
     boardSquare.id = i;
-    boardSquare.textContent = createGameBoard.gameBoard[i];
     gameBoardContainer.appendChild(boardSquare);
   }
 }
 
 renderGameBoard();
 
-// Factory function to generate a private variable count to keep this safe from cheating,
-// which increments by 1 each time function is called
-function incrementByOne() {
+const incrementByOne = () => {
   count++;
-}
+};
 
 gameBoardContainer.addEventListener("click", (e) => {
   // Checks which player's turn it should be using a counter,
   // even is playerOne, odd playerTwo,
   // note use array methods and the target id to update the appropriate array index
   incrementByOne();
-  console.log(count);
   const targetSquare = document.getElementById(e.target.id);
-  if (count % 2 === 0) {
+  const index = targetSquare.id;
+  if (count % 2 !== 0) {
     targetSquare.textContent = playerOne.marker;
+    gameBoard.gameBoardArray[index] = playerOne.marker;
+    console.log("playerOne");
+    console.log(count);
   } else {
     targetSquare.textContent = playerTwo.marker;
+    gameBoard.gameBoardArray[index] = playerTwo.marker;
+    console.log("playerTwo");
+    console.log(count);
   }
 });
