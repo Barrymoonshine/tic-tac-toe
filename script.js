2c3cbe61afc026b15928b54b9fcbe34855ff7a05
Nick Sheward <nickshwrd@gmail.com>
2023-02-22 15:20:29 +0000

    Player counter defined as a private variable

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const gameBoardContainer = document.getElementById("game-board-container");
-let count = 0;
 const winningPatterns = [
   [0, 1, 2],
   [3, 4, 5],
@@ -11,50 +10,60 @@ const winningPatterns = [
   [2, 4, 6],
 ];
 
-// Module pattern to generate the game board and array
-const gameBoard = (function () {
+const generateGameBoard = (() => {
+  // Module pattern and loop to generate the game board and array
+  const gameBoardArray = [];
   for (i = 0; i < 9; i++) {
     gameBoardContainer.innerHTML += String.raw`
     <div class="board-squares" id='${i}'>
     `;
+    gameBoardArray.push("");
   }
-  const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
   return {
     gameBoardArray,
   };
 })();
 
 // Factory function to generate players
-const playerFactory = (name, marker) => ({ name, marker });
-const playerOne = playerFactory("playerOne", "X");
-const playerTwo = playerFactory("playerTwo", "O");
+const generatePlayers = (name, marker) => ({ name, marker });
+const playerOne = generatePlayers("playerOne", "X");
+const playerTwo = generatePlayers("playerTwo", "O");
 
 gameBoardContainer.addEventListener("click", handleGameFlow);
 
-function handleGameFlow(e) {
-  // Checks which player's using a counter, even is playerOne, odd playerTwo
-  // Checks if a square has already been used
+const switchPlayer = (() => {
+  // Module pattern to switch players between rounds
+  let playerCount = 0;
+  function changeBy(val) {
+    playerCount += val;
+  }
+  return {
+    switch() {
+      changeBy(1);
+    },
+    checkPlayer() {
+      return playerCount;
+    },
+  };
+})();
 
+function handleGameFlow(e) {
   const targetSquare = document.getElementById(e.target.id);
   const index = targetSquare.id;
   if (targetSquare.textContent == "X" || targetSquare.textContent == "O") {
-  } else if (count % 2 === 0) {
+  } else if (switchPlayer.checkPlayer() % 2 === 0) {
     targetSquare.textContent = playerOne.marker;
-    gameBoard.gameBoardArray[index] = playerOne.marker;
+    generateGameBoard.gameBoardArray[index] = playerOne.marker;
   } else {
     targetSquare.textContent = playerTwo.marker;
-    gameBoard.gameBoardArray[index] = playerTwo.marker;
+    generateGameBoard.gameBoardArray[index] = playerTwo.marker;
   }
   checkForWinner();
-  incrementByOne();
+  switchPlayer.switch();
 }
 
-const incrementByOne = () => {
-  count++;
-};
-
 function checkForWinner() {
-  const currentArray = gameBoard.gameBoardArray;
+  const currentArray = generateGameBoard.gameBoardArray;
   // Loops through the game board array,
   // And returns the index values for both players in a nested array
   const returnIndexes = currentArray.reduce(
@@ -68,18 +77,18 @@ function checkForWinner() {
     },
     [[], []]
   );
-  // Loops through winning index positions
-  // Returns congratulations message to winner
+  // Compares each players index position with the winning patterns
+  // Displays message if there is a winner
   winningPatterns.forEach((array) => {
-    if (multipleInArray(returnIndexes[0], array) === true) {
+    if (checkWinningPattern(returnIndexes[0], array) === true) {
       alert("Player one has won!");
-    } else if (multipleInArray(returnIndexes[1], array) === true) {
+    } else if (checkWinningPattern(returnIndexes[1], array) === true) {
       alert("Player two has won!");
     }
   });
 }
 
-function multipleInArray(indexes, array) {
+function checkWinningPattern(indexes, array) {
   // Loops through the winning index values to check,
   // if they are contained in the current game board array
   // Re-usable function for checking multiple winning patterns
